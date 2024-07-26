eb4e0c23af220a7f7e5e2072bb3214f07283302b
aleroom <aleroom@yahoo.com>
2024-07-26 14:39:44 +0300

    tailwind free form

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,22 +4,66 @@ import {useForm} from 'react-hook-form'
 function App() {
   const{
     register,
-    formState: {
-      errors
-    },
-    handleSubmit
-  } = useForm();
+    formState: {errors, isValid},
+    handleSubmit, 
+    reset,
+  } = useForm({
+    mode: "onBlur"
+  })
 
-    const onSubmit = (data) => {}
+
+    const onSubmit = (data) => {
       alert(JSON.stringify(data))
+      reset();
+    }
+      
   return (
     <div className='App'>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          {...register('')}
-        />
+        <label >        
+          Login
+          <input
+            {...register("login", {
+              required: "Поле обязательно к заполнению",
+              minLength: {
+                value: 3,
+                message: "Минимум 3 символа"
+              },
+              maxLength: {
+                value: 7,
+                message: "Максимум 7 символов"
+              }
 
-        <input type='submit'/>
+            })}
+          />
+        </label>
+
+        <label >        
+          Password
+          <input
+            {...register("password", {
+              required: "Поле обязательно к заполнению",
+              minLength: {
+                value: 6,
+                message: "Минимум 6 символа"
+              },
+              maxLength: {
+                value: 9,
+                message: "Максимум 9 символов"
+              }
+
+            })}
+          />
+        </label>
+         <div>
+          {errors?.login && <p>{errors?.login?.message || "!Error"}</p>} 
+          {errors?.password && <p>{errors?.password?.message || "!Error"}</p>} 
+         </div>
+        
+          <input type='submit' name="submit" disabled={!isValid}/>
+          
+
+
       </form>
     </div>
   );
--- my-app/src/tailwind.config.js
+++ my-app/src/tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    content: ["./index.html"],
+    content: ["./src/**/*.{html,js}"],
     theme: {
       extend: {},
     },
