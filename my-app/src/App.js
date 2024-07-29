fe85fd1d48d9e5fd88c78dce585363b23d3dcb5b
aleroom <aleroom@yahoo.com>
2024-07-29 16:43:32 +0300

    add tailwind

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,12 +18,17 @@ function App() {
     }
       
   return (
-    <div className='App'>
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <div class="mx-auto max-w-xl text-center">
+      <div > 
+        <h2 class="text-3xl font-bold tracking-tight text-gray-700 sm:text-4xl">Registration form</h2>
+      </div>
       
-        <label  >        
+      <form class="mx-auto mt-16 max-w-xl sm:mt-20" onSubmit={handleSubmit(onSubmit)} >
+      
+        <label class="block text-lg font-semibold leading-6 text-gray-900"  >        
           Login
-          <input
+          
+          <input class="mx-auto block w-1/2  rounded-md border-2 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
             {...register("login", {
               required: "This field is required",
               minLength: {
@@ -39,9 +44,9 @@ function App() {
           />
         </label>
 
-        <label>        
+        <label class="block text-lg font-semibold leading-6 text-gray-900">        
           Password
-           <input
+           <input class="mx-auto block w-1/2  rounded-md border-2 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mb-8"
             {...register("password", {
               required: "This field is required",
               minLength: {
@@ -61,7 +66,7 @@ function App() {
           {errors?.password && <p>{errors?.password?.message || "!Error"}</p>} 
          </div>
         
-          <input type='submit' name="submit" disabled={!isValid}/>
+          <input type='submit' name="submit" class="mx-auto block w-1/2 rounded-md bg-gray-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-orange-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 " disabled={!isValid}/>
           
 
 
